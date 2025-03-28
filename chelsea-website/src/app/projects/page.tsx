81fe7718a0463339e1648a8d80252f7ee32d4aa1
Chelsea Rice <officialchelsearice@gmail.com>
2025-03-28 17:14:31 -0400

    folders and different page created
    created simple functional nav bar for page

---
+++ chelsea-website/src/app/projects/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Projects() {
+    return (
+      <div className="relative min-h-screen bg-[#cdd837]">
+                  {/* Navigation Bar */}
+                  <nav className="absolute top-0 right-0 p-6">
+                    <ul className="flex gap-6 text-lg font-medium">
+                      <li>
+                        <a href="/home" className="hover:underline">
+                          Home
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/projects" className="hover:underline">
+                          Projects
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/contact" className="hover:underline">
+                          Contact
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+            
+                  {/* Logo centered at the top */}
+                  <div className="flex justify-center items-start pt-8">
+                    <Image src="/CLogo.png" alt="Logo" width={180} height={38} priority />
+                  </div>
+                </div>
+    );
+  }
+  
